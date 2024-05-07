cc400d71f979a2fbb359dd162989048e02e07bdc
Александр Осипов <alikxyzet@gmail.com>
2024-05-08 00:56:14 +0300

    4.5 New Base

--- M4.cpp
+++ M4.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int subrtact(int a, int b)
+{
+	return a - b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
 int main()
 {
+	int a;
+	std::cout << "Enter a number: ";
+	std::cin >> a;
+
+	int b;
+	std::cout << "Enter another number: ";
+	std::cin >> b;
+
+	int op;
+	do
+	{
+		std::cout << "Enter an operation (0 = add, 1 = subrtact, 2 = multiply): ";
+		std::cin >> op;
+	} while (0 > op || op > 2);
+
+	int result = 0;
+	switch (op)
+	{
+	case 0: result = add(a, b); break;
+	case 1: result = subrtact(a, b); break;
+	case 2: result = multiply(a, b); break;
+	}
+
+	std::cout << "The answer is: " << result << std::endl;
 
 	return 0;
 }
