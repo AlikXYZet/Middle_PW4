790b7769272691e16fce4a893f819271e41d9b41
Александр Осипов <alikxyzet@gmail.com>
2024-05-07 22:55:45 +0300

    4.2 "Virtual functions (links)"

--- M4.cpp
+++ M4.cpp
@@ -1,44 +1,63 @@
 #include <iostream>
 
-void boo(double*& p)
+void foo(int& ref)
 {
-
+	ref = 10;
 }
 
-struct Test1
+void foo2(int* S)
 {
+	*S = 11;
+}
 
+struct Something
+{
+	int value1 = 5;
+	float value2 = 6.2;
 };
 
-class Test2
+struct Other
 {
-
+	Something something;
+	int otherValue;
 };
+
+Other other;
 
 int main()
 {
-	int b = 10;
-	int a = 100;
-	std::cout << &b << "\n";
-	std::cout << b << "\n";
-	std::cout << *&b << "\n";
+	int Seven = 7;
+	int& Ref = Seven;
 
-	int* Ptr = &b;
-	Ptr = &a;
-	std::cout << Ptr << "\n";
-	std::cout << *Ptr << "\n";
+	std::cout << Ref << '\n';
 
-	double testD = 0.00000001;
-	double* PtrD = &testD;
-	boo(PtrD);
-	std::cout << PtrD << "\n";
-	std::cout << *PtrD << "\n";
+	Ref++;
+	Seven++;
 
-	int* TestI_1;
-	Test2* TestI_2;
-	char* TestC_1;
+	std::cout << Seven << '\n';
+	std::cout << Ref << '\n';
 
-	std::cout << sizeof(TestI_1) << "\n";
-	std::cout << sizeof(TestI_2) << "\n";
-	std::cout << sizeof(TestC_1) << "\n";
+	const int SeventySeven = 77;
+	const int& Ref2 = SeventySeven;
+
+	int value1 = 8;
+	int value2 = 20;
+
+	std::cout << value1 << '\n';
+	std::cout << value2 << '\n';
+
+	foo(value1);
+	foo2(&value2);
+
+	std::cout << value1 << '\n';
+	std::cout << value2 << '\n';
+
+	int& ref1 = value1;
+	ref1 = value2;
+
+	std::cout << '\n';
+
+	int& refSomething = other.something.value1;
+	std::cout << other.something.value1 << '\n';
+	std::cout << refSomething << '\n';
 }
