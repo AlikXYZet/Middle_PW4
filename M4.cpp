73a1cac68740c16211a688cf56d88b09efb1fc99
Александр Осипов <alikxyzet@gmail.com>
2024-05-08 01:51:36 +0300

    4.6.2 New Base

--- M4.cpp
+++ M4.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+class IErrorLog
+{
+public:
+	virtual bool openLog(const char* fileName) = 0;
+	virtual bool closeLog() = 0;
+	virtual bool writeError(const char* errorMessage) = 0;
+
+	virtual ~IErrorLog() {};
+};
+
 int main()
 {
 
