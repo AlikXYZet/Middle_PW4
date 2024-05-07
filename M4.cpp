99de0212a07ac893b716d42a5ed058d491921907
Александр Осипов <alikxyzet@gmail.com>
2024-05-07 23:32:01 +0300

    4.3 New Base

--- M4.cpp
+++ M4.cpp
@@ -1,32 +1,7 @@
 #include <iostream>
 
-class Parent
-{
-protected:
-	int m_value;
-
-public:
-	Parent(int value) : m_value(value)
-	{
-	}
-
-	const char* getName() { return "Parent"; }
-	int getValue() { return m_value; }
-};
-
-class Child : public Parent
-{
-public:
-	Child(int value) : Parent(value)
-	{
-	}
-
-	const char* getName() { return "Child"; }
-	int getValue() { return m_value * 2; }
-};
-
-
 int main()
 {
 
+	return 0;
 }
