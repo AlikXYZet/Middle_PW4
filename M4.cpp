8135b202c55cca4f3677174db6fab7322c698a12
Александр Осипов <alikxyzet@gmail.com>
2024-05-07 23:52:05 +0300

    4.3.2 Base

--- M4.cpp
+++ M4.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+class Animal
+{
+protected:
+	std::string m_name;
+	Animal(std::string name) : m_name(name)
+	{
+	}
+
+public:
+	std::string getName() { return m_name; }
+	const char* speak() { return "???"; }
+};
+
+class Cat : public Animal
+{
+public:
+	Cat(std::string name) : Animal(name)
+	{
+	}
+
+	const char* speak() { return "Meow"; }
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(std::string name) : Animal(name)
+	{
+	}
+
+	const char* speak() { return "Woof"; }
+};
+
 int main()
 {
 
