644846151ee3a9e1180706361ea03d35388b751e
Александр Осипов <alikxyzet@gmail.com>
2024-05-07 23:28:00 +0300

    4.3.1 Example 1 ["Virtual functions (inheritance)"]

--- M4.cpp
+++ M4.cpp
@@ -22,11 +22,29 @@ public:
 	}
 
 	const char* getName() { return "Child"; }
-	int getValue() { return m_value * 2; }
+	int getValueDoubled() { return m_value * 2; }
 };
 
 
 int main()
 {
+	Child child(5);
+	std::cout << "child Name: " << child.getName() << ";   Has value : " << child.getValue() << '\n';
 
+	Child& rChild = child;
+	std::cout << "rChild Name: " << rChild.getName() << ";   Has value : " << rChild.getValue() << '\n';
+
+	Child* pChild = &child;
+	std::cout << "pChild Name: " << pChild->getName() << ";   Has value : " << pChild->getValue() << '\n';
+	
+	std::cout << '\n';
+
+	Parent& rParent = child;
+	Parent* pParent = &child;
+
+	std::cout << "child Name: " << child.getName() << ";   Has value : " << child.getValue() << '\n';
+	std::cout << "rParent Name: " << rParent.getName() << ";   Has value : " << rParent.getValue() << '\n';
+	std::cout << "pParent Name: " << pParent->getName() << ";   Has value : " << pParent->getValue() << '\n';
+
+	return 0;
 }
