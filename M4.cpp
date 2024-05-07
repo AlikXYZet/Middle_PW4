d4e59fc16cd3bd12f014c9229e6589452882b726
Александр Осипов <alikxyzet@gmail.com>
2024-05-08 02:00:23 +0300

    4.6.2 Example 2 ["Pure Virtual Functions, Interfaces, and Abstract Classes"]

--- M4.cpp
+++ M4.cpp
@@ -10,6 +10,27 @@ public:
 	virtual ~IErrorLog() {};
 };
 
+class FileErrorLog : public IErrorLog
+{
+
+};
+
+class ConsoleErrorLog : public IErrorLog
+{
+
+};
+
+double mySqrt(double value, IErrorLog& log)
+{
+	if (value < 0.0)
+	{
+		log.writeError("Tried to take square root of value less than 0");
+		return 0.0;
+	}
+	else
+		return sqrt(value);
+}
+
 int main()
 {
 
