eaa1da0f02cb993d0b95b1651287a6868dd9f056
Александр Осипов <alikxyzet@gmail.com>
2024-05-08 00:28:25 +0300

    4.4 New Base

--- M4.cpp
+++ M4.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 
+class Parent
+{
+public:
+	~Parent()
+	{
+		std::cout << "Calling ~Parent()" << std::endl;
+	}
+};
+
+class Child : public Parent
+{
+private:
+	int* m_array;
+
+public:
+	Child(int length)
+	{
+		m_array = new int[length];
+	}
+
+	~Child()
+	{
+		std::cout << "Calling ~Child()" << std::endl;
+		delete[] m_array;
+	}
+};
+
+
 int main()
 {
+	Child* child = new Child(9);
+	Parent* parent = child;
+	delete parent;
 
 	return 0;
 }
