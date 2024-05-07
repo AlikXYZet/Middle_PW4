66050e5604c50cf8d1f9af729b1bc944bdb9c5bd
Александр Осипов <alikxyzet@gmail.com>
2024-05-07 22:11:12 +0300

    4.1 "Virtual functions (pointers)"

--- M4.cpp
+++ M4.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
-int main()
+void boo(double*& p)
 {
 
+}
+
+struct Test1
+{
+
+};
+
+class Test2
+{
+
+};
+
+int main()
+{
+	int b = 10;
+	int a = 100;
+	std::cout << &b << "\n";
+	std::cout << b << "\n";
+	std::cout << *&b << "\n";
+
+	int* Ptr = &b;
+	Ptr = &a;
+	std::cout << Ptr << "\n";
+	std::cout << *Ptr << "\n";
+
+	double testD = 0.00000001;
+	double* PtrD = &testD;
+	boo(PtrD);
+	std::cout << PtrD << "\n";
+	std::cout << *PtrD << "\n";
+
+	int* TestI_1;
+	Test2* TestI_2;
+	char* TestC_1;
+
+	std::cout << sizeof(TestI_1) << "\n";
+	std::cout << sizeof(TestI_2) << "\n";
+	std::cout << sizeof(TestC_1) << "\n";
 }
