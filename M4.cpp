881d5e631adbf4a93e47dca5ba1416d7da76408c
Александр Осипов <alikxyzet@gmail.com>
2024-05-08 04:46:23 +0300

    PW4.A Base (Overloading the "print()" function)

--- M4.cpp
+++ M4.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+
+
 int main()
 {
+	Car c(Engine(150), Wheel(17), Wheel(17), Wheel(18), Wheel(18), 150);
+	std::cout << c << '\n';
 
+	Bicycle t(Wheel(20), Wheel(20), 300);
+	std::cout << t << '\n';
+
+	return 0;
 }
